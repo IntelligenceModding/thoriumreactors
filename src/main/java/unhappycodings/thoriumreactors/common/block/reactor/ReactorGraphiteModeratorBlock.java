37a3a192311436992df3674dbc7c3692b425888f
UnhappyCodings <gannatic.german@gmail.com>
2023-04-23 00:06:17 +0200

    + Optimized reactor moderator to 9x9 base

--- src/main/java/unhappycodings/thoriumreactors/common/block/reactor/ReactorGraphiteModeratorBlock.java
+++ src/main/java/unhappycodings/thoriumreactors/common/block/reactor/ReactorGraphiteModeratorBlock.java
@@ -12,8 +12,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class ReactorGraphiteModeratorBlock extends GlassBlock {
 
-    public static final VoxelShape ALL = Shapes.or(Block.box(7, 0, 7, 9, 16, 9), Block.box(10, 0, 4, 12, 16, 6),
-            Block.box(10, 0, 10, 12, 16, 12), Block.box(4, 0, 10, 6, 16, 12), Block.box(4, 0, 4, 6, 16, 6));
+    public static final VoxelShape ALL = Block.box(4, 0, 4, 11.5, 16, 11.5);
 
     public ReactorGraphiteModeratorBlock() {
         super(Properties.of(Material.STONE));
