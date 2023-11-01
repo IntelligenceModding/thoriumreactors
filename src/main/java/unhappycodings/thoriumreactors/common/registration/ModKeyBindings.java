de0f2d8c2827f231eb5772bfe5279f1fd87e7643
UnhappyCodings <gannatic.german@gmail.com>
2023-11-01 22:58:13 +0100

    + Fixed wrong keybindings category name

--- src/main/java/unhappycodings/thoriumreactors/common/registration/ModKeyBindings.java
+++ src/main/java/unhappycodings/thoriumreactors/common/registration/ModKeyBindings.java
@@ -9,7 +9,7 @@ import unhappycodings.thoriumreactors.ThoriumReactors;
 
 public class ModKeyBindings {
 
-    public static final KeyMapping SHOW_DETAILS = new KeyMapping("keybind." + ThoriumReactors.MOD_ID + ".details", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(GLFW.GLFW_KEY_LEFT_SHIFT), "itemGroup." + ThoriumReactors.MOD_ID + ".items");
-    public static final KeyMapping SHOW_DESCRIPTION = new KeyMapping("keybind." + ThoriumReactors.MOD_ID + ".description", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(GLFW.GLFW_KEY_LEFT_CONTROL), "itemGroup." + ThoriumReactors.MOD_ID + ".items");
+    public static final KeyMapping SHOW_DETAILS = new KeyMapping("keybind." + ThoriumReactors.MOD_ID + ".details", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(GLFW.GLFW_KEY_LEFT_SHIFT), "itemGroup." + ThoriumReactors.MOD_ID);
+    public static final KeyMapping SHOW_DESCRIPTION = new KeyMapping("keybind." + ThoriumReactors.MOD_ID + ".description", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(GLFW.GLFW_KEY_LEFT_CONTROL), "itemGroup." + ThoriumReactors.MOD_ID);
 
 }
