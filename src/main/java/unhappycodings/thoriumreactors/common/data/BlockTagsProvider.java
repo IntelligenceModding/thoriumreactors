88712ea16dc102811066008a562d99e42f47b650
UnhappyCodings <gannatic.german@gmail.com>
2024-03-03 21:27:45 +0100

    + Fixed many recipes to now include tags instead of firm items fixing #18 + Added missing tags to block and items

--- src/main/java/unhappycodings/thoriumreactors/common/data/BlockTagsProvider.java
+++ src/main/java/unhappycodings/thoriumreactors/common/data/BlockTagsProvider.java
@@ -2,11 +2,13 @@ package unhappycodings.thoriumreactors.common.data;
 
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 import unhappycodings.thoriumreactors.ThoriumReactors;
@@ -31,6 +33,13 @@ public class BlockTagsProvider extends net.minecraft.data.tags.TagsProvider<Bloc
             tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block.get());
             tag(BlockTags.NEEDS_STONE_TOOL).add(block.get());
         }
+        Block[] stoneList = {ModBlocks.MANGANESE_ORE.get(), ModBlocks.CHROMITE_ORE.get(), ModBlocks.MOLYBDENUM_ORE.get(), ModBlocks.NICKEL_ORE.get(), ModBlocks.TITANIC_IRON_ORE.get(), ModBlocks.BAUXITE_ORE.get(), ModBlocks.PYROCHLOR_ORE.get(), ModBlocks.URANIUM_ORE.get(), ModBlocks.GRAPHITE_ORE.get(), ModBlocks.FLUORITE_ORE.get()};
+        Block[] deepslateList = {ModBlocks.DEEPSLATE_MANGANESE_ORE.get(), ModBlocks.DEEPSLATE_CHROMITE_ORE.get(), ModBlocks.DEEPSLATE_MOLYBDENUM_ORE.get(), ModBlocks.DEEPSLATE_NICKEL_ORE.get(), ModBlocks.DEEPSLATE_TITANIC_IRON_ORE.get(), ModBlocks.DEEPSLATE_BAUXITE_ORE.get(), ModBlocks.DEEPSLATE_PYROCHLOR_ORE.get(), ModBlocks.DEEPSLATE_URANIUM_ORE.get(), ModBlocks.DEEPSLATE_GRAPHITE_ORE.get(), ModBlocks.DEEPSLATE_FLUORITE_ORE.get()};
+
+        for (int i = 0; i < stoneList.length; i++) {
+            tag(create(new ResourceLocation("forge", "ores"))).add(getBlockResourceKey(stoneList[i]), getBlockResourceKey(deepslateList[i]));
+        }
+
 
         tag(create(new ResourceLocation("forge", "ores/manganese"))).add(ModBlocks.MANGANESE_ORE.get(), ModBlocks.DEEPSLATE_MANGANESE_ORE.get());
         tag(create(new ResourceLocation("forge", "ores/chromite"))).add(ModBlocks.CHROMITE_ORE.get(), ModBlocks.DEEPSLATE_CHROMITE_ORE.get());
@@ -45,6 +54,13 @@ public class BlockTagsProvider extends net.minecraft.data.tags.TagsProvider<Bloc
         tag(create(new ResourceLocation("forge", "ores/uranium"))).add(ModBlocks.URANIUM_ORE.get(), ModBlocks.DEEPSLATE_URANIUM_ORE.get());
         tag(create(new ResourceLocation("forge", "ores/graphite"))).add(ModBlocks.GRAPHITE_ORE.get(), ModBlocks.DEEPSLATE_GRAPHITE_ORE.get());
         tag(create(new ResourceLocation("forge", "ores/fluorite"))).add(ModBlocks.FLUORITE_ORE.get(), ModBlocks.DEEPSLATE_FLUORITE_ORE.get());
+
+        RegistryObject[] blocks = {ModBlocks.DEEPSLATE_TITANIC_IRON_ORE, ModBlocks.TITANIC_IRON_ORE, ModBlocks.DEEPSLATE_CHROMITE_ORE, ModBlocks.CHROMITE_ORE, ModBlocks.DEEPSLATE_GRAPHITE_ORE, ModBlocks.GRAPHITE_ORE, ModBlocks.DEEPSLATE_MOLYBDENUM_ORE, ModBlocks.MOLYBDENUM_ORE, ModBlocks.DEEPSLATE_FLUORITE_ORE, ModBlocks.FLUORITE_ORE, ModBlocks.DEEPSLATE_NICKEL_ORE, ModBlocks.NICKEL_ORE, ModBlocks.DEEPSLATE_URANIUM_ORE, ModBlocks.URANIUM_ORE, ModBlocks.DEEPSLATE_PYROCHLOR_ORE, ModBlocks.PYROCHLOR_ORE, ModBlocks.DEEPSLATE_MANGANESE_ORE, ModBlocks.MANGANESE_ORE, ModBlocks.DEEPSLATE_BAUXITE_ORE, ModBlocks.BAUXITE_ORE};
+
+        for (RegistryObject<Block> block : blocks) {
+            tag(create(new ResourceLocation("forge", "ores_in_ground/" + (block.get().getName().getString().contains("deepslate") ? "deepslate" : "stone")))).add(getResourceKey(block));
+            tag(create(new ResourceLocation("forge", "ores"))).add(getResourceKey(block));
+        }
 
         tag(create(new ResourceLocation("forge", "storage_blocks/chromium"))).add(ModBlocks.CHROMIUM_BLOCK.get());
         tag(create(new ResourceLocation("forge", "storage_blocks/graphite"))).add(ModBlocks.GRAPHITE_BLOCK.get());
@@ -61,6 +77,16 @@ public class BlockTagsProvider extends net.minecraft.data.tags.TagsProvider<Bloc
         tag(create(new ResourceLocation("forge", "storage_blocks/aluminium"))).add(ModBlocks.ALUMINUM_BLOCK.get());
         tag(create(new ResourceLocation("forge", "storage_blocks/manganese"))).add(ModBlocks.MANGANESE_BLOCK.get());
         tag(create(new ResourceLocation("forge", "storage_blocks/pyrochlore"))).add(ModBlocks.NIOB_BLOCK.get());
+        tag(create(new ResourceLocation("forge", "storage_blocks/thorium"))).add(getResourceKey(ModBlocks.THORIUM_BLOCK));
+
+        tag(create(new ResourceLocation("forge", "chests/thorium"))).add(getResourceKey(ModBlocks.THORIUM_CHEST_BLOCK));
+        tag(create(new ResourceLocation("forge", "chests/steel"))).add(getResourceKey(ModBlocks.STEEL_CHEST_BLOCK));
+        tag(create(new ResourceLocation("forge", "chests/blasted_iron"))).add(getResourceKey(ModBlocks.BLASTED_IRON_CHEST_BLOCK));
+        tag(create(new ResourceLocation("forge", "chests"))).add(getResourceKey(ModBlocks.THORIUM_CHEST_BLOCK)).add(getResourceKey(ModBlocks.STEEL_CHEST_BLOCK)).add(getResourceKey(ModBlocks.BLASTED_IRON_CHEST_BLOCK));
+
+        tag(create(new ResourceLocation("forge", "ore_rates/singular"))).add(getResourceKey(ModBlocks.URANIUM_ORE)).add(getResourceKey(ModBlocks.DEEPSLATE_URANIUM_ORE));
+        tag(create(new ResourceLocation("forge", "ore_rates/dense"))).add(getResourceKey(ModBlocks.GRAPHITE_ORE)).add(getResourceKey(ModBlocks.DEEPSLATE_GRAPHITE_ORE));
+        tag(create(new ResourceLocation("forge", "ore_rates/dense"))).add(getResourceKey(ModBlocks.FLUORITE_ORE)).add(getResourceKey(ModBlocks.DEEPSLATE_FLUORITE_ORE));
     }
 
     public static TagKey<Block> create(ResourceLocation name) {
@@ -71,6 +97,14 @@ public class BlockTagsProvider extends net.minecraft.data.tags.TagsProvider<Bloc
     @Override
     protected Path getPath(ResourceLocation location) {
         return this.generator.getOutputFolder().resolve("data/" + location.getNamespace() + "/tags/blocks/" + location.getPath() + ".json");
+    }
+
+    public ResourceKey<Block> getResourceKey(RegistryObject<? extends Block> block) {
+        return ForgeRegistries.BLOCKS.getResourceKey(block.get()).get();
+    }
+
+    public ResourceKey<Block> getBlockResourceKey(Block block) {
+        return ForgeRegistries.BLOCKS.getResourceKey(block).get();
     }
 
     @NotNull
--- src/main/java/unhappycodings/thoriumreactors/common/data/ItemTagsProvider.java
+++ src/main/java/unhappycodings/thoriumreactors/common/data/ItemTagsProvider.java
@@ -2,13 +2,16 @@ package unhappycodings.thoriumreactors.common.data;
 
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 import unhappycodings.thoriumreactors.ThoriumReactors;
+import unhappycodings.thoriumreactors.common.registration.ModBlocks;
 import unhappycodings.thoriumreactors.common.registration.ModItems;
 import unhappycodings.thoriumreactors.common.util.ItemUtil;
 
@@ -35,14 +38,54 @@ public class ItemTagsProvider extends net.minecraft.data.tags.TagsProvider<Item>
         Item[] ingotList = {ModItems.GRAPHITE_INGOT.get(), ModItems.BLASTED_IRON_INGOT.get(), ModItems.STEEL_INGOT.get(), ModItems.MOLYBDENUM_INGOT.get(), ModItems.MANGANESE_INGOT.get(), ModItems.NICKEL_INGOT.get(), ModItems.ALUMINUM_INGOT.get(), ModItems.CHROMIUM_INGOT.get(), ModItems.NIOB_INGOT.get(), ModItems.TITANIUM_INGOT.get(), ModItems.URANIUM_INGOT.get(), ModItems.FLUORITE_INGOT.get(), ModItems.COBALT_INGOT.get()};
         Item[] nuggetList = {ModItems.GRAPHITE_NUGGET.get(), ModItems.BLASTED_IRON_NUGGET.get(), ModItems.STEEL_NUGGET.get(), ModItems.MOLYBDENUM_NUGGET.get(), ModItems.MANGANESE_NUGGET.get(), ModItems.NICKEL_NUGGET.get(), ModItems.ALUMINUM_NUGGET.get(), ModItems.CHROMIUM_NUGGET.get(), ModItems.NIOB_NUGGET.get(), ModItems.TITANIUM_NUGGET.get(), ModItems.URANIUM_NUGGET.get(), ModItems.FLUORITE_NUGGET.get(), ModItems.COBALT_NUGGET.get()};
 
+        // Ingots
         for (Item item : ingotList) {
             String current = ItemUtil.getRegString(item).replace("thoriumreactors:", "").replace("_ingot", "");
-            tag(ItemTags.create(new ResourceLocation("forge", "ingots/" + current))).add(item);
+            tag(ItemTags.create(new ResourceLocation("forge", "ingots/" + current))).add(getResourceKey(item));
+            tag(ItemTags.create(new ResourceLocation("forge", "ingots"))).add(getResourceKey(item));
         }
+        tag(ItemTags.create(new ResourceLocation("forge", "ingots/aluminium"))).add(getResourceKey(ModItems.ALUMINUM_INGOT.get()));
+        tag(ItemTags.create(new ResourceLocation("forge", "ingots/chrome"))).add(getResourceKey(ModItems.CHROMIUM_INGOT.get()));
+
+        // Nuggets
         for (Item item : nuggetList) {
             String current = ItemUtil.getRegString(item).replace("thoriumreactors:", "").replace("_nugget", "");
-            tag(ItemTags.create(new ResourceLocation("forge", "nuggets/" + current))).add(item);
+            tag(ItemTags.create(new ResourceLocation("forge", "nuggets/" + current))).add(getResourceKey(item));
+            tag(ItemTags.create(new ResourceLocation("forge", "nuggets"))).add(getResourceKey(item));
         }
+        tag(ItemTags.create(new ResourceLocation("forge", "nuggets/aluminium"))).add(getResourceKey(ModItems.ALUMINUM_NUGGET.get()));
+        tag(ItemTags.create(new ResourceLocation("forge", "nuggets/chrome"))).add(getResourceKey(ModItems.CHROMIUM_NUGGET.get()));
+
+        // Gems
+        tag(ItemTags.create(new ResourceLocation("forge", "gems/fluorite"))).add(getResourceKey(ModItems.FLUORITE.get()));
+        tag(ItemTags.create(new ResourceLocation("forge", "gems"))).add(getResourceKey(ModItems.FLUORITE.get()));
+
+        tag(ItemTags.create(new ResourceLocation("forge", "yellow_cake_uranium"))).add(getResourceKey(ModItems.YELLOW_CAKE.get()));
+
+        // Raw Materials
+        tag(ItemTags.create(new ResourceLocation("forge", "raw_materials/uranium"))).add(getResourceKey(ModItems.RAW_URANIUM.get()));
+        tag(ItemTags.create(new ResourceLocation("forge", "raw_materials/graphite"))).add(getResourceKey(ModItems.GRAPHITE_CRYSTAL.get()));
+        tag(ItemTags.create(new ResourceLocation("forge", "raw_materials/sodium"))).add(getResourceKey(ModItems.SODIUM.get()));
+        tag(ItemTags.create(new ResourceLocation("forge", "raw_materials/potassium"))).add(getResourceKey(ModItems.POTASSIUM.get()));
+        tag(ItemTags.create(new ResourceLocation("forge", "raw_materials/thorium"))).add(getResourceKey(ModItems.THORIUM.get()));
+
+        // Storage Blocks
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/chromium"))).add(getResourceKey(ModBlocks.CHROMIUM_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/graphite"))).add(getResourceKey(ModBlocks.GRAPHITE_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/steel"))).add(getResourceKey(ModBlocks.STEEL_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/blasted_iron"))).add(getResourceKey(ModBlocks.BLASTED_IRON_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/titanic_iron"))).add(getResourceKey(ModBlocks.TITANIUM_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/titanium"))).add(getResourceKey(ModBlocks.TITANIUM_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/molybdenum"))).add(getResourceKey(ModBlocks.MOLYBDENUM_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/cobalt"))).add(getResourceKey(ModBlocks.COBALT_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/fluorite"))).add(getResourceKey(ModBlocks.FLUORITE_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/nickel"))).add(getResourceKey(ModBlocks.NICKEL_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/uranium"))).add(getResourceKey(ModBlocks.URANIUM_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/aluminum"))).add(getResourceKey(ModBlocks.ALUMINUM_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/aluminium"))).add(getResourceKey(ModBlocks.ALUMINUM_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/manganese"))).add(getResourceKey(ModBlocks.MANGANESE_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/pyrochlore"))).add(getResourceKey(ModBlocks.NIOB_BLOCK.get().asItem()));
+        tag(ItemTags.create(new ResourceLocation("forge", "storage_blocks/thorium"))).add(getResourceKey(ModBlocks.THORIUM_BLOCK.get().asItem()));
 
     }
 
@@ -50,6 +93,10 @@ public class ItemTagsProvider extends net.minecraft.data.tags.TagsProvider<Item>
     @Override
     protected Path getPath(ResourceLocation location) {
         return this.generator.getOutputFolder().resolve("data/" + location.getNamespace() + "/tags/items/" + location.getPath() + ".json");
+    }
+
+    public ResourceKey<Item> getResourceKey(Item item) {
+        return ForgeRegistries.ITEMS.getResourceKey(item).get();
     }
 
     @NotNull
